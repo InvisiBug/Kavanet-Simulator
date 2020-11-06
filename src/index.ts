9eed7b55fc93f80263e9e57563f98003a5b6f304
InvisiBug <matthewjkavanagh@gmail.com>
2020-11-06 13:23:54 +0000

    Fixed prettier issue

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 console.log("Hello World");
-setInterval(()=> console.log("Helloooooo"), 1000)
+setInterval(() => console.log("Helloooooo"), 1000);
+
+if (true) {
+  console.log("do something");
+}
+
+if (true) {
+  console.log("do something else");
+}
+
+if (false) {
+  console.log("kjsadhksjh");
+}
