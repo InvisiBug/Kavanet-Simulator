a6e95519624e8b64a145db41920f3563355112ca
InvisiBug <matthewjkavanagh@gmail.com>
2021-12-20 00:54:35 +0000

    Added types to devices

--- src/components/devices/components/rgbLights.ts
+++ src/components/devices/components/rgbLights.ts
@@ -48,6 +48,7 @@ export default class RBGLight {
       this.topic,
       JSON.stringify({
         node: this.name,
+        type: "rgbLights",
         red: this.red,
         green: this.green,
         blue: this.blue,
