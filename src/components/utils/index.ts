e4bbaba7736215faea3b539ce7b84d1e169b47b2
InvisiBug <matthewjkavanagh@gmail.com>
2021-12-30 21:24:36 +0000

    wip

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -25,3 +25,11 @@ export const publishOnConnect = () => {
 };
 
 export { mqttUrl } from "./mqttUrlGenerator";
+
+// import * as dotenv from "dotenv";
+// import dotenvExpand from "dotenv-expand";
+// var myEnv = dotenv.config();
+// dotenvExpand(myEnv);
+
+// export const mongoUrl: string = process.env.MONGO ?? "";
+// export const mqttUrl: string = process.env.MQTT ?? "";
