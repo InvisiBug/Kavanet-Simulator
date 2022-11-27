f3694a280267bda0f0eb3d70ea3b5ede941948ec
InvisiBug <matthewjkavanagh@gmail.com>
2022-11-27 20:01:55 +0000

    Added radiator v2

--- src/components/devices/index.ts
+++ src/components/devices/index.ts
@@ -4,3 +4,4 @@ export { default as ComputerAudio } from "./components/computerAudio";
 export { default as RBGLights } from "./components/rgbLights";
 export { default as Valves } from "./components/valves";
 export { default as Plugs } from "./components/plugs";
+export { default as Radiator } from "./components/radiator";
